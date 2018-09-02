d86673fbf3ac168c53d537c786c202ba6401218d
prateek-parashar <parashar4111@gmail.com>
2018-09-02 19:31:40 +0530

    Implementation of the Deque data structure

--- src/LinkedListDeque.java
+++ src/LinkedListDeque.java
@@ -12,6 +12,13 @@ public class LinkedListDeque {
             next = n;
             prev = p;
         }
+        /** Returns the size of the internal implementation */
+        private int get(int index) {
+            if (index == 0) {
+                return this.data;
+            }
+            else return this.next.get(index - 1);
+        }
     }
 
     // sentinel attribute (the middle man)
@@ -39,6 +46,62 @@ public class LinkedListDeque {
         size++;
     }
 
+    /** Checks if the Deque is empty */
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Returns the size of the Deque */
+    public int size() {
+        return this.size;
+    }
+
+    /** Prints the Deque is order */
+    public void printDeque() {
+        Node p = this.sentinel;
+        while (p.next != sentinel) {
+            System.out.print(p.next.data + " ");
+            p = p.next;
+        }
+    }
+
+    /** Removes and returns the first item of the Deque */
+    public int returnFirst() {
+        if (size == 0) {
+            return 0;
+        }
+        else {
+            Node p = sentinel.next;
+            sentinel.next = p.next;
+            p.next.prev = sentinel;
+            return p.data;
+        }
+    }
+
+    /** Returns and removes the last element of the Deque */
+    public int returnLast() {
+        if (size == 0) {
+            return 0;
+        }
+        else {
+            Node p = sentinel.prev;
+            sentinel.prev = p.prev;
+            p.prev.next = sentinel;
+            return p.data;
+        }
+    }
+
+    /** Returns the item at a given index */
+    public int get(int x) {
+        if (size == 0) {
+            return 0;
+        }
+        return sentinel.next.get(x);
+    }
+
     public static void main (String[] args){
         LinkedListDeque L = new LinkedListDeque();
         L.addFirst(15);
@@ -46,6 +109,8 @@ public class LinkedListDeque {
         L.addFirst(5);
         L.addLast(25);
         L.addLast(30);
-
+        L.printDeque();
+        System.out.println("");
+        System.out.print(L.get(4));
     }
 }
