2ec549c23d7d8c4632a7b646cc366df7e1591ce4
prateek-parashar <parashar4111@gmail.com>
2018-09-07 22:21:47 +0530

    completed implementation of the test based development

--- Testing/src/Sort.java
+++ Testing/src/Sort.java
@@ -1,6 +1,39 @@
 public class Sort {
     /** Sorts a given array destructively */
-    public static String[] sort(String[] input) {
-        return null;
+    public static void sort(String[] input) {
+        /** Implementation of Selection Sort
+         * 1. Find the smallest item
+         * 2. Swap it with the first item
+         * 3. DO Selection sort on the remaining array
+         */
+        sort(input, 0);
+    }
+
+    /** private method that allows us to use recursion */
+    private static void sort(String[] input, int start) {
+        if (start == input.length) {
+            return;
+        }
+        int smallestIndex = findSmallest(input, start);
+        swap(input, start, smallestIndex);
+        sort(input, start + 1);
+    }
+
+    /** Returns the index of the smallest value in the array */
+    public static int findSmallest(String [] input, int start) {
+        int smallest = start;
+        for (int i = start; i < input.length; i++) {
+            if (input[smallest].compareTo(input[i]) > 0) {
+                smallest = i;
+            }
+        }
+        return smallest;
+    }
+
+    /** Swaps the given indexes of the array */
+    public static void swap(String[] input, int a, int b) {
+        String temp = input[a];
+        input[a] = input[b];
+        input[b] = temp;
     }
 }
--- Testing/src/TestSort.java
+++ Testing/src/TestSort.java
@@ -1,19 +1,33 @@
 public class TestSort {
+    /** Test for sort Method */
     public static void testSort() {
         String[] input = {"this", "is", "bloody", "awesome"};
         String[] expected = {"awesome", "bloody", "is", "this"};
         Sort.sort(input);
 
-        for (int i = 0; i < input.length; i++) {
-            if (!input[i].equals(expected[i])) {
-                System.out.println("Error! expected " + expected[i] + " received " + input[i]);
-                return;
-            }
-        }
-
+        org.junit.Assert.assertArrayEquals(expected, input);
     }
 
+    /** Tests the method to find the smallest value */
+    public static void testFindSmallet() {
+        String[] input = {"this", "is", "bloody", "awesome"};
+        int expected = 3;
+        int actual = Sort.findSmallest(input, 0);
+        org.junit.Assert.assertEquals(expected, actual);
+    }
+
+    /** Test the method to swap values */
+    public static void testSwap() {
+        String[] input = {"this", "is", "bloody", "awesome"};
+        int a = 0;
+        int b = 2;
+        String[] expected = {"bloody", "is", "this", "awesome"};
+        Sort.swap(input, a, b);
+        org.junit.Assert.assertArrayEquals(expected, input);
+    }
+
+
     public static void main(String args[]) {
-        TestSort.testSort();
+        testSort();
     }
 }
