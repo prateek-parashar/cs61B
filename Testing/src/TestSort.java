197a62cb4099e14d25a71d212b60db7efd1e3d1b
prateek-parashar <parashar4111@gmail.com>
2018-09-07 23:45:16 +0530

    added import statements to the jUnit library

--- Testing/src/TestSort.java
+++ Testing/src/TestSort.java
@@ -1,33 +1,35 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class TestSort {
     /** Test for sort Method */
-    public static void testSort() {
+    @Test
+    public void testSort() {
         String[] input = {"this", "is", "bloody", "awesome"};
         String[] expected = {"awesome", "bloody", "is", "this"};
         Sort.sort(input);
 
-        org.junit.Assert.assertArrayEquals(expected, input);
+        assertArrayEquals(expected, input);
     }
 
     /** Tests the method to find the smallest value */
-    public static void testFindSmallet() {
+    @Test
+    public void testFindSmallest() {
         String[] input = {"this", "is", "bloody", "awesome"};
         int expected = 3;
         int actual = Sort.findSmallest(input, 0);
-        org.junit.Assert.assertEquals(expected, actual);
+        assertEquals(expected, actual);
     }
 
     /** Test the method to swap values */
-    public static void testSwap() {
+    @Test
+    public void testSwap() {
         String[] input = {"this", "is", "bloody", "awesome"};
         int a = 0;
         int b = 2;
         String[] expected = {"bloody", "is", "this", "awesome"};
         Sort.swap(input, a, b);
-        org.junit.Assert.assertArrayEquals(expected, input);
+        assertArrayEquals(expected, input);
     }
 
-
-    public static void main(String args[]) {
-        testSort();
-    }
 }
